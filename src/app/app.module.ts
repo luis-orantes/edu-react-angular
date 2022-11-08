9bd69453ce0c89a15156d5ee9e58399f0e9f0fd2
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2022-11-07 19:46:10 -0700

    login and register components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { RentalModule } from './rentals/rental.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     RentalModule,
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bwm-register',
+  template: `
+    <p>
+      register works!
+    </p>
+  `,
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
