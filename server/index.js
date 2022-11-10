bdb8cdb857521ef1b58e50ff043e3ed6efc24445
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2022-11-09 20:58:09 -0700

    api. searching rentals in json

--- server/index.js
+++ server/index.js
@@ -3,8 +3,27 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const rentals = [
+    {
+      _id: '2137129312',
+      city: 'New York',
+      title: 'Very nice place'
+    },
+    {
+      _id: 'asjkdajdasnda',
+      city: 'Berlin',
+      title: 'Very nice place as well!'
+    },
+  ]
+
 app.get('/api/v1/rentals', (req, res) => {
-    return res.send('hi');
+    return res.json(rentals);
+})
+
+app.get('/api/v1/rentals/:rentalId', (req, res) => {
+    const {rentalId } = req.params;
+    const rental = rentals.find(item => item._id === req.params.rentalId);
+    return res.json(rental);
 })
 
 app.listen(PORT, () => {
