c06e96daea2e51d728648f42a76654dd2a2ea6fa
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2022-11-09 20:29:19 -0700

    api. return string

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get('/api/v1/rentals', (req, res) => {
+    return res.send('hi');
+})
+
 app.listen(PORT, () => {
     console.log('listening on port: ' + PORT);
 })
