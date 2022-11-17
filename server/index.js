8eeeb5f02db1f8b2a01747521f4d971db13c0b02
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2022-11-16 21:46:57 -0700

    delete, update to memory json

--- server/index.js
+++ server/index.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { find } = require('tslint/lib/utils');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const rentals = [
-    {
-      _id: '2137129312',
-      city: 'New York',
-      title: 'Very nice place'
-    },
-    {
-      _id: 'asjkdajdasnda',
-      city: 'Berlin',
-      title: 'Very nice place as well!'
-    },
+  {
+    _id: '2137129312',
+    city: 'New York',
+    title: 'Very nice place'
+  },
+  {
+    _id: 'asjkdajdasnda',
+    city: 'Berlin',
+    title: 'Very nice place as well!'
+  },
   ]
+
+function rentalsFind(rentalId) {
+  return rentals.findIndex(item => item._id === rentalId);
+}
 
 app.use(bodyParser.json());
 
 app.get('/api/v1/rentals', (req, res) => {
-    return res.json(rentals);
+  return res.json(rentals);
 })
 
 app.get('/api/v1/rentals/:rentalId', (req, res) => {
-    const {rentalId } = req.params;
-    const rental = rentals.find(item => item._id === req.params.rentalId);
-    return res.json(rental);
+  const { rentalId } = req.params;
+  const rental = rentals.find(item => item._id === req.params.rentalId);
+  return res.json(rental);
 })
 
 app.post('/api/v1/rentals', (req, res) => {
@@ -35,6 +40,40 @@ app.post('/api/v1/rentals', (req, res) => {
   res.json({
     message: `rental with id ${rentalData._id} was added!` 
   });
+})
+
+app.delete('/api/v1/rentals/:rentalId', (req, res) => {
+  const { rentalId } = req.params;
+  const id = rentalsFind(rentalId);
+  if(id >= 0) {
+    rentals.splice(id, 1);
+    res.json({
+      message: `rental with id ${rentalId} was deleted!` 
+    });
+  } else {
+    res.json({
+      message: `ERROR: rental with id ${rentalId} was not found!` 
+    });
+  }
+
+})
+
+app.patch('/api/v1/rentals/:rentalId', (req, res) => {
+  const { rentalId } = req.params;
+  const id = rentalsFind(rentalId);
+  const rentalData = req.body;
+  if(id >= 0) {
+    rentals[id].city = rentalData.city;
+    rentals[id].title = rentalData.title;
+    res.json({
+      message: `rental with id ${rentalId} was updated!` 
+    });
+  } else {
+    res.json({
+      message: `ERROR: rental with id ${rentalId} was not found!` 
+    });
+  }
+
 })
 
 app.listen(PORT, () => {
