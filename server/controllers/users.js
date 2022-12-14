5585305f0e574a22b8e7a534b71ee5227fafc99e
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2022-12-13 22:08:40 -0700

    fix. register controller is invoking twice

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -32,17 +32,15 @@ exports.register = (req, res) => {
     }
     if(userExisting) {
       return res.status(422).send({error: {title: 'Error in registering', detail: 'Username already exists'}});
-      return; // TODO> *** Use this line to avoid crashing the server when username already exists. The controller is invoking twice when this error occurs.
     }
+
+    const usernameNew = new users({ username, password, email });
+
+    usernameNew.save((error, userCreated) => {
+      if(error) {
+        return res.status(422).send({error: {title: 'Error in registering', detail: 'Internal DB error'}});
+      }
+      return res.json({message: `New username registered with id: ${userCreated._id}`});
+    })
   })
-
-  const usernameNew = new users({ username, password, email });
-
-  usernameNew.save((error, userCreated) => {
-    if(error) {
-      return res.status(422).send({error: {title: 'Error in registering', detail: 'Internal DB error'}});
-    }
-    return res.json({message: `New username registered with id: ${userCreated._id}`});
-  })
-
 }
