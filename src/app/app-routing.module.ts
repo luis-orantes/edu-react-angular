1fce6e6221a06c8e5078ea32bae8d4f52ab3662a
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2023-02-24 00:02:08 -0700

    secret page checks if auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuard } from './auth/shared/auth.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: '/rentals', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
--- src/app/auth/shared/auth.guard.ts
+++ src/app/auth/shared/auth.guard.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +18,40 @@ import { Observable } from 'rxjs';
 export class AuthGuard implements CanActivate {
 
   private url: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
   
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     this.url = state.url;
+    return this.authService.isAuthenticated ?
+      this.authTrueValidate() : this.authFalseValidate();
+  }
+
+  private authTrueValidate(): boolean {
+    if(this.isAuthPage) {
+      this.router.navigate(['/rentals']);
+      return false;
+    }
     return true;
   }
-  
+
+  private authFalseValidate(): boolean {
+    if(this.isAuthPage)
+      return true;
+    this.router.navigate(['/login']);
+    return false
+  }
+
+  private get isAuthPage(): boolean {
+    return this.url.includes('login') || this.url.includes('register') ? true : false;
+  }
+
+
+
+
 }
