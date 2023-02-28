12d0b1ff33fa2c007959a493c939711dbd520cc3
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2023-02-27 21:15:00 -0700

    map is displaying with no location

--- src/app/shared/modules/map/map/map.component.ts
+++ src/app/shared/modules/map/map/map.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import tt from '@tomtom-international/web-sdk-maps';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'bwm-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class MapComponent implements OnInit {
+export class MapComponent {
 
-  @Input('location') location: string;
+  private readonly TT_API_KEY = '';
+  @Input('location') set location (location: string) {
+    this.createMap();
+  };
 
   constructor() { }
 
-  ngOnInit() {
+  createMap() {
+    const map = tt.map({
+      key: this.TT_API_KEY,
+      container: 'bwm-map',
+      style: 'tomtom://vector/1/basic-main',
+    });
+    map.addControl(new tt.NavigationControl());
   }
+
+
+
 
 }
