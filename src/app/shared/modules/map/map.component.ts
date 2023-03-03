4a55afa66ddd90230753c77b3f0693b013f49315
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2023-03-02 23:07:37 -0700

    moving tomtom calls from component to service

--- src/app/shared/modules/map/map.component.ts
+++ src/app/shared/modules/map/map.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
-import tt from '@tomtom-international/web-sdk-maps';
 import { map } from 'rxjs/operators';
 
-import { AppConfig } from 'src/app/shared/app-config';
 import { MapService } from './map.service';
 
 @Component({
@@ -12,51 +10,27 @@ import { MapService } from './map.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class MapComponent {
+  
+  private map: any;
 
   @Input('location') set location (location: string) {
-    this.createMap();
+    this.map = this.mapService.createMap();
     // this.getGeolocation('ajsldfklakdjsf'); // to test an invalid location
     this.getGeolocation(location);
   };
-
-  private map: any;
 
   constructor(
     private mapService: MapService,
   ) { }
 
-  createMap() {
-    this.map = tt.map({
-      key: AppConfig.TT_API_KEY,
-      container: 'bwm-map',
-      style: 'tomtom://vector/1/basic-main',
-      zoom: 15,
-      scrollZoom: false,
-    });
-    // this.map.addControl(new tt.NavigationControl());
-  }
+
 
   private getGeolocation(location: string) {
     this.mapService.reqGeoLocation(location)
       .subscribe(res => {
-        this.map.setCenter(new tt.LngLat(res.lon, res.lat));
-
-        const markerDiv = document.createElement('div');
-        markerDiv.className = 'bwm-marker';
-        new tt.Marker({
-          element: markerDiv,
-        })
-          .setLngLat([res.lon, res.lat])
-          .addTo(this.map);
+        this.mapService.initMap(this.map, res);
     }, (err: Error) => {
-      new tt.Popup({
-        className: 'bwm-popup',
-        closeButton: false,
-        closeOnClick: false,
-      })
-        .setLngLat(new tt.LngLat(0,0))
-        .setHTML(`<p>${err.message}</p>`)
-        .addTo(this.map)
+      this.mapService.addPopupToMap(this.map, err.message);
     }
     );
   }
--- src/app/shared/modules/map/map.service.ts
+++ src/app/shared/modules/map/map.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
+import tt from '@tomtom-international/web-sdk-maps';
 
 import { AppConfig } from '../../app-config';
 
@@ -41,6 +42,44 @@ export class MapService {
 
   private get locationErr() {
     return new Error('Location not found!');
+  }
+
+  createMap() {
+    return tt.map({
+      key: AppConfig.TT_API_KEY,
+      container: 'bwm-map',
+      style: 'tomtom://vector/1/basic-main',
+      zoom: 15,
+      scrollZoom: false,
+    });
+    // this.map.addControl(new tt.NavigationControl());
+  }
+
+  initMap(map: any, position: positionRes) {
+    this.centerMap(map, position);
+    this.addMarkerToMap(map, position);
+  }
+
+  centerMap(map: any, position: positionRes) {
+    map.setCenter(new tt.LngLat(position.lon, position.lat));
+  }
+
+  addMarkerToMap(map: any, position: positionRes) {
+    const markerDiv = document.createElement('div');
+    markerDiv.className = 'bwm-marker';
+
+    new tt.Marker({
+      element: markerDiv
+    })
+      .setLngLat([position.lon, position.lat])
+      .addTo(map);
+  }
+
+  addPopupToMap(map: any, message: string) {
+    new tt.Popup({className: 'bwm-popup', closeButton: false, closeOnClick: false})
+      .setLngLat(new tt.LngLat(0, 0))
+      .setHTML(`<p>${message}</p>`)
+      .addTo(map);
   }
 
 
