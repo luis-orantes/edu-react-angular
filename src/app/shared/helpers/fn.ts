1f53d2e36c79ce03369ba324e95996240d33e00c
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2023-03-22 15:25:54 -0700

    EXTRA: detect unformatted error from api

--- src/app/shared/helpers/fn.ts
+++ src/app/shared/helpers/fn.ts
@@ -4,11 +4,23 @@ import { throwError } from "rxjs";
 
 
 export function extractApiErr (resErr: HttpErrorResponse): BwmApi.errs[] {
-  let errs = [{title: 'Error', msg: 'Some error ocurred'}];
+  let errs: BwmApi.errs[] = [{title: 'Error', msg: 'Some error ocurred'}];
   
   if(resErr && resErr.error && resErr.error.err) {
     errs = resErr.error.err;
   }
+
+  if(!errs[0].title || !errs[0].msg) {
+    // titles not shown
+    errs=[
+      {
+        title: 'ERROR',
+        msg: 'UNFORMATTED ERROR: ',
+      }, {
+        title: 'ERROR',
+        msg: JSON.stringify(errs),
+      }];
+    }
   
   return errs;
 }
