19f29ec4105f88b5a07e0eafaaf76827f02a8084
Luis Orantes <89365852+orionGalactic@users.noreply.github.com>
2023-04-06 12:28:24 -0700

    EXTA: fix to db err message format

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -15,7 +15,7 @@ exports.dbErrMid = (req, res, next) => {
     if(err && prop in err && err.name === "ValidationError") {
       const errMsg = err[prop];
       for(let key in errMsg) {
-        result.push({key: errMsg[key].message});
+        result.push({title: key, msg: errMsg[key].message});
       }
     } else {
       return res.status(422).send({title: "dbErr", msg: "Error in dbErr"});
