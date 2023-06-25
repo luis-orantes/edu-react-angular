5dee0f4fd5157410198de71a4dd0d7f46361449d
Luis Orantes <89365852+luis-orantes@users.noreply.github.com>
2023-06-25 10:44:45 -0700

    booking model

--- src/app/rentals/components/rental-booking/rental-booking/rental-booking.component.ts
+++ src/app/rentals/components/rental-booking/rental-booking/rental-booking.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Moment } from 'moment';
 
+import { Booking } from 'src/app/booking/shared/booking.model';
+
 @Component({
   selector: 'bwm-rental-booking',
   templateUrl: './rental-booking.component.html',
@@ -9,18 +11,32 @@ import { Moment } from 'moment';
 export class RentalBookingComponent implements OnInit {
 
   @Input('isAuth') isAuth = false;
-  selected: { startDate: Moment, endDate: Moment };
+  calendar: { startDate: Moment, endDate: Moment };
   locale = {
     format: "YYYY/MM/DD",
-  }
+  };
+  newBooking: Booking;
   
   constructor() { }
 
   ngOnInit() {
+    this.initBooking();
+  }
+
+  initBooking() {
+    this.newBooking = new Booking();
+    this.newBooking.guests = 1;
+  }
+
+  updateBookingDates({startDate, endDate}: {[key: string]: Moment}) {
+    if(!startDate || !endDate) { return; };
+
+    this.newBooking.startAt = startDate.format();
+    this.newBooking.endAt = endDate.format();
   }
 
   reservePlace() {
-    alert(JSON.stringify(this.selected));
+    alert(JSON.stringify(this.newBooking));
   }
 
 
